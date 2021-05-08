f820d0ffdd75dcc70e1a8943d427c16c6eef4196
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-05-08 09:03:36 -0500

    additional data files

---
+++ data-setup/real-time-tracking.js
@@ -0,0 +1,10 @@
+db = db.getSiblingDB("learn");
+
+db.tracking.insertMany({
+        "coordinates": [
+            -122.4252,
+            37.9397
+        ],
+        "username": "rrajesh"
+    }
+);
