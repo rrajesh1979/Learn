14dfea7aa5591ad94606f1957026a6f6b11291d9
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-06-30 07:28:06 -0500

    AOP

--- learn-springboot/src/main/java/com/learn/springboot/aop/LoggingAspect.java
+++ learn-springboot/src/main/java/com/learn/springboot/aop/LoggingAspect.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
